9993303d75dc8e4510eca3113772e0ec5dd252df
unknown <wb.lifajin@net.ntes>
2018-07-26 11:22:20 +0800

    报告中添加视频功能

--- uiautomator2/ext/htmlreport/assets/simplehttpserver.py
+++ uiautomator2/ext/htmlreport/assets/simplehttpserver.py
@@ -5,6 +5,7 @@ import six
 import socket
 from contextlib import closing
 import webbrowser
+import imageio, os
 
 if six.PY2:
     import SimpleHTTPServer
@@ -29,7 +30,17 @@ def free_port():
         return s.getsockname()[1]
 
 
+def img2video():
+    images = []
+    pwd = os.path.dirname(os.path.abspath(__file__))
+    files = sorted(( pwd + '\\imgs\\' + pic for pic in os.listdir('.\\imgs') if pic.endswith('.jpg')))
+    for filename in files:
+        images.append(imageio.imread(filename))
+    imageio.mimsave('.\\imgs\\video.mp4', images, fps=1)
+
+
 def main():
+    img2video()
     PORT = free_port()
     Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
     httpd = SocketServer.TCPServer(("", PORT), Handler)
