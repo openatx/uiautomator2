ebe8f1be03b56f866130e5415d8d7681204b159b
unknown <wb.lifajin@net.ntes>
2018-08-27 10:04:51 +0800

    1.fix html

--- uiautomator2/ext/htmlreport/assets/simplehttpserver.py
+++ uiautomator2/ext/htmlreport/assets/simplehttpserver.py
@@ -5,7 +5,7 @@ import six
 import socket
 from contextlib import closing
 import webbrowser
-import imageio, os
+import os
 
 if six.PY2:
     import SimpleHTTPServer
@@ -30,17 +30,7 @@ def free_port():
         return s.getsockname()[1]
 
 
-def img2video():
-    images = []
-    pwd = os.path.dirname(os.path.abspath(__file__))
-    files = sorted(( pwd + '\\imgs\\' + pic for pic in os.listdir('.\\imgs') if pic.endswith('.jpg')))
-    for filename in files:
-        images.append(imageio.imread(filename))
-    imageio.mimsave('.\\imgs\\video.mp4', images, fps=1)
-
-
 def main():
-    img2video()
     PORT = free_port()
     Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
     httpd = SocketServer.TCPServer(("", PORT), Handler)
