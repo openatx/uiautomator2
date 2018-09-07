5de283b30fc67fe431a68ddb2704f536e4fe4451
codeskyblue <codeskyblue@gmail.com>
2018-09-07 16:22:56 +0800

    add traffic into perf plugin

--- uiautomator2/ext/perf/README.md
+++ uiautomator2/ext/perf/README.md
@@ -1,5 +1,5 @@
 # Performance 性能采集
-自动记录测试过程中的CPU，PSS
+自动记录测试过程中的CPU，PSS, NET
 
 使用方法
 ```python
@@ -13,12 +13,45 @@ u2.plugin_register('perf', perf.Perf)
 def main():
     d = u2.connect()
     d.ext_perf.package_name = package_name
+    d.ext_perf.csv_output = "perf.csv" # 保存数据到perf.csv
+    # d.debug = True # 采集到数据就输出，默认关闭
+    # d.interval = 1.0 # 数据采集间隔，默认1.0s，尽量不要小于0.5s，因为采集内存比较费时间
     d.ext_perf.start()
 
-    # run ... tests
-    d.ext_perf.stop() # call at last
+    # run ... tests code here ...
+    d.ext_perf.stop() # 最好结束的时候调用下，虽然不调用也没多大关系
 
 
 if __name__ == '__main__':
     main()
 ```
+
+保存的csv文件内容格式为
+
+```csv
+time,pss,cpu,systemCpu,rxBytes,txBytes
+2018-09-07 16:09:16.725,161.41,1.9,6.2,0,0
+2018-09-07 16:09:17.812,161.42,3.6,9.1,31650,2943
+2018-09-07 16:09:19.043,167.1,17.3,52.5,3379507,133231
+2018-09-07 16:09:19.800,168.92,12.8,33.6,801161,30242
+2018-09-07 16:09:20.871,168.78,12.3,26.0,0,0
+2018-09-07 16:09:21.842,168.76,12.1,26.9,0,0
+2018-09-07 16:09:22.910,169.39,12.5,26.3,187,64
+```
+
+数据项说明
+
+- PSS直接通过`dumpsys meminfo <package-name>`获取
+- CPU应该是会超过100%的，直接读取的`/proc/`下的文件计算出来的
+- rxBytes, txBytes 目前只有wlan的流量，tcp和udp的流量总和
+
+## 参考资料
+- [Python CSV读写方法](https://python3-cookbook.readthedocs.io/zh_CN/latest/c06/p01_read_write_csv_data.html)
+- [android屏幕刷新显示机制](https://blog.csdn.net/litefish/article/details/53939882)
+- [Android FPS计算方法](https://www.jianshu.com/p/1fe9783d266b)
+- [Github项目@leekinwa-androidTestTools_performance_FPS](https://github.com/leekinwa/androidTestTools_Performance_FPS)
+- [官方proc文件格式资料](http://man7.org/linux/man-pages/man5/proc.5.html)
+- [Chromium有关FPS的计算方法](https://github.com/ChromiumWebApps/chromium/blob/master/build/android/pylib/perf/surface_stats_collector.py)
+- [FPS 计算方法的比较 by fenfenzhong](https://testerhome.com/topics/4643)
+- [安卓性能测试之cpu占用率统计方法总结](https://www.jianshu.com/p/6bf564f7cdf0)
+- [Android 性能测试实践 (四) 流量](https://testerhome.com/topics/2643)
--- uiautomator2/ext/perf/__init__.py
+++ uiautomator2/ext/perf/__init__.py
@@ -6,21 +6,37 @@ from __future__ import absolute_import, print_function
 import threading
 import re
 import time
+import datetime
 import csv
+import atexit
+from collections import namedtuple
 
 _MEM_PATTERN = re.compile(r'TOTAL[:\s]+(\d+)')
+# acct_tag_hex is a socket tag
+# cnt_set==0 are for background data
+# cnt_set==1 are for foreground data
+_NetStats = namedtuple(
+    "NetStats",
+    """idx iface acct_tag_hex uid_tag_int cnt_set rx_bytes rx_packets
+    tx_bytes tx_packets rx_tcp_bytes rx_tcp_packets rx_udp_bytes rx_udp_packets rx_other_bytes rx_other_packets
+    tx_tcp_bytes tx_tcp_packets tx_udp_bytes tx_udp_packets tx_other_bytes tx_other_packets"""
+    .split())
 
 
 class Perf(object):
     def __init__(self, d, package_name=None):
         self.d = d
         self.package_name = package_name
+        self.csv_output = "perf.csv"
+        self.debug = False
+        self.interval = 1.0
         self._th = None
         self._event = threading.Event()
         self._condition = threading.Condition()
+        self._data = {}
 
     def memory(self):
-        """ PSS """
+        """ PSS(KB) """
         output = self.d.shell(['dumpsys', 'meminfo', self.package_name]).output
         m = _MEM_PATTERN.search(output)
         if m:
@@ -34,51 +50,39 @@ class Perf(object):
         ds = list(map(int, first_line.split()[1:]))
         total_cpu = sum(ds)
         idle = ds[3]
-        # print("total cpu", total_cpu)
 
         proc_stat = d.shell(
             ['cat', '/proc/%d/stat' % pid]).output.split(') ')[1].split()
         utime = int(proc_stat[11])
         stime = int(proc_stat[12])
         return (total_cpu, idle, utime + stime)
-        # print(proc_stat)
-        # print(utime, stime)
 
-    def cpu(self):
+    def cpu(self, pid):
         """ CPU
 
         Refs:
         - http://man7.org/linux/man-pages/man5/proc.5.html
         - [安卓性能测试之cpu占用率统计方法总结](https://www.jianshu.com/p/6bf564f7cdf0)
         """
-        pid = self.d._pidof_app(self.package_name)
-        if pid is None:
-            return 0.0
-        print("Pid:", pid)
-        tjiff1, idle1, pjiff1 = self._cpu_rawdata_collect(pid)
-        time.sleep(.3)
-        tjiff2, idle2, pjiff2 = self._cpu_rawdata_collect(pid)
+        store_key = 'cpu-%d' % pid
+        # first time jiffies
+        if store_key in self._data:
+            tjiff1, idle1, pjiff1 = self._data[store_key]
+        else:
+            tjiff1, idle1, pjiff1 = self._cpu_rawdata_collect(pid)
+            time.sleep(.3)
+
+        # second time jiffies
+        self._data[
+            store_key] = tjiff2, idle2, pjiff2 = self._cpu_rawdata_collect(pid)
+
+        # calculate
         pcpu = 100.0 * (pjiff2 - pjiff1) / (tjiff2 - tjiff1)  # process cpu
         scpu = 100.0 * ((tjiff2 - idle2) -
                         (tjiff1 - idle1)) / (tjiff2 - tjiff1)  # system cpu
-        # print("CPU:", pcpu, scpu)
         return round(pcpu, 1), round(scpu, 1)
-        # print("cal cpu:", round(pcpu, 1), round(scpu, 1))
 
-        # first_line = d.shell(['cat', '/proc/stat']).output.splitlines()[0]
-        # assert first_line.startswith('cpu ')
-        # total_cpu = sum(map(int, first_line.split()[1:]))
-        # print("total cpu", total_cpu)
-
-        # proc_stat = d.shell(
-        #     ['cat', '/proc/%d/stat' % pid]).output.split(') ')[1].split()
-        # utime = proc_stat[11]
-        # stime = proc_stat[12]
-        # print(proc_stat)
-        # print(utime, stime)
-
-        # time.sleep(.3)
-
+        # Retrive cpu from top
         # for line in d.shell(["top", "-n", "1"]).output.splitlines():
         #     vs = line.split()
         #     if len(vs) > 5 and vs[-1] == self.package_name:
@@ -87,71 +91,126 @@ class Perf(object):
         #             return float(vs[4][:-1]), 0.0
         # return (0.0, 0.0)
 
+    def netstat(self, pid):
+        """
+        Returns:
+            (rx_bytes, tx_bytes)
+        """
+        m = re.search(r'^Uid:\s+(\d+)',
+                      self.d.shell(['cat', '/proc/%d/status' % pid]).output,
+                      re.M)
+        if not m:
+            return (0, 0)
+        uid = m.group(1)
+        lines = self.d.shell(
+            ['cat', '/proc/net/xt_qtaguid/stats']).output.splitlines()
+
+        rx, tx = 0, 0
+        for line in lines:
+            vs = line.split()
+            if len(vs) != 21:
+                continue
+            v = _NetStats(*vs)
+            if v.uid_tag_int != uid:
+                continue
+            if v.iface != 'wlan0':
+                continue
+            # FIXME(ssx): tcp and udp data will support when some one needed
+            rx += int(v.rx_bytes)
+            tx += int(v.tx_bytes)
+
+        store_key = 'netstat-%s' % uid
+        drx, dtx = 0, 0
+        if store_key in self._data:
+            last_rx, last_tx = self._data[store_key]
+            drx, dtx = rx - last_rx, tx - last_tx
+        self._data[store_key] = (rx, tx)
+        return drx, dtx
+
     def collect(self):
-        start = time.time()
+        pid = self.d._pidof_app(self.package_name)
+        if pid is None:
+            return
         pss = self.memory()
-        print("memofy time used =", time.time() - start)
-        cpu, scpu = self.cpu()
-        print("collect time used =", time.time() - start)
+        cpu, scpu = self.cpu(pid)
+        rx_bytes, tx_bytes = self.netstat(pid)
+        timestr = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
         return {
-            'time': int(time.time() * 1000),
-            'pss': pss,
+            'time': timestr,
+            'pss': round(pss / 1024.0, 2),
             'cpu': cpu,
             'systemCpu': scpu,
+            'rxBytes': rx_bytes,
+            'txBytes': tx_bytes,
         }
 
-    def continue_collect(self):
+    def continue_collect(self, f):
         try:
-            with open("perf.csv", "w", newline='\n') as f:
-                fcsv = csv.writer(f)
-                headers = ['time', 'pss', 'cpu', 'systemCpu']
-                fcsv.writerow(headers)
-                while not self._event.isSet():
-                    perfdata = self.collect()
-                    fcsv.writerow([perfdata[k] for k in headers])
-                    print(time.time(), perfdata)
-                    time.sleep(.5)
+            headers = ['time', 'pss', 'cpu', 'systemCpu', 'rxBytes', 'txBytes']
+            fcsv = csv.writer(f)
+            fcsv.writerow(headers)
+            update_time = time.time()
+            while not self._event.isSet():
+                perfdata = self.collect()
+                if self.debug:
+                    print("DEBUG:", perfdata)
+                if not perfdata:
+                    print("perf package is not alive:", self.package_name)
+                    time.sleep(1)
+                    continue
+                fcsv.writerow([perfdata[k] for k in headers])
+                wait_seconds = max(0,
+                                   self.interval - (time.time() - update_time))
+                time.sleep(wait_seconds)
+                update_time = time.time()
+            f.close()
         finally:
-            print("collect finish")
             self._condition.acquire()
             self._th = None
-            print("notify condi")
             self._condition.notify()
             self._condition.release()
 
     def start(self):
+        f = open("perf.csv", "w", newline='\n')
+
+        def defer_close():
+            if not f.closed:
+                f.close()
+
+        atexit.register(defer_close)
+
         if self._th:
             raise RuntimeError("perf is already running")
         if not self.package_name:
             raise EnvironmentError("package_name need to be set")
+        self._data.clear()
         self._event = threading.Event()
         self._condition = threading.Condition()
-        self._th = threading.Thread(target=self.continue_collect)
+        self._th = threading.Thread(target=self.continue_collect, args=(f, ))
         self._th.daemon = True
         self._th.start()
 
     def stop(self):
-        print("stop() wait collect stopped")
         self._event.set()
         self._condition.acquire()
-        print("wait done")
         self._condition.wait(timeout=2)
         self._condition.release()
-        print("stoped")
 
 
 if __name__ == '__main__':
     import uiautomator2 as u2
     pkgname = "com.tencent.tmgp.sgame"
+    pkgname = "com.netease.cloudmusic"
     u2.plugin_register('perf', Perf, pkgname)
 
     d = u2.connect("10.242.62.224")
     print(d.current_app())
+    # print(d.ext_perf.netstat(5350))
     # d.app_start(pkgname)
     d.ext_perf.start()
+    d.ext_perf.debug = True
     try:
         time.sleep(500)
     except KeyboardInterrupt:
         d.ext_perf.stop()
         print("threading stopped")
-    # time.sleep(2)
