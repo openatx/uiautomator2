cd64083e0a105b62259a94037be1685063bf9abd
codeskyblue <codeskyblue@gmail.com>
2019-06-22 16:19:28 +0800

    fix debug curl print

--- uiautomator2/__init__.py
+++ uiautomator2/__init__.py
@@ -222,7 +222,7 @@ class TimeoutRequestsSession(requests.Session):
             print(
                 datetime.now().strftime("%H:%M:%S.%f")[:-3],
                 "$ curl -X {method} -d '{data}' '{url}'".format(
-                    method=method, url=url, data=data))
+                    method=method, url=url, data=data.decode()))
         try:
             resp = super(TimeoutRequestsSession, self).request(
                 method, url, **kwargs)
