578d73ec64546717abf8c6e7c993b03750d462ef
Vancheung <vancheung129@163.com>
2021-05-26 11:24:31 +0800

    fix: 增加app_install的超时设置，防止因安装未完成而出现exit_code=-1

--- uiautomator2/__init__.py
+++ uiautomator2/__init__.py
@@ -1326,8 +1326,8 @@ class _AppMixIn:
         """
         target = "/data/local/tmp/_tmp.apk"
         self.push(data, target, show_progress=True)
-        logger.debug("pm install -rt %s", target)
-        ret = self.shell(['pm', 'install', "-r", "-t", target])
+        logger.debug("pm install -r -t %s", target)
+        ret = self.shell(['pm', 'install', "-r", "-t", target],timeout=300)
         if ret.exit_code != 0:
             raise RuntimeError(ret.output, ret.exit_code)
 
