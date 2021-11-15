4adde3c4da20408e3111bf775f99dc86f99e2702
codeskyblue <codeskyblue@gmail.com>
2021-11-15 14:07:43 +0800

    update again

--- uiautomator2/assets/sync.sh
+++ uiautomator2/assets/sync.sh
@@ -8,11 +8,11 @@ APK_VERSION="2.3.3"
 
 cd "$(dirname $0)"
 
-if test -f apk_version.txt -a "$APK_VERSION" = "$(cat apk_version.txt)"
-then
-	echo "apk version $APK_VERSION, already downloaded, exit"
-	exit
-fi
+#if test -f apk_version.txt -a "$APK_VERSION" = "$(cat apk_version.txt)"
+#then
+#	echo "apk version $APK_VERSION, already downloaded, exit"
+#	exit
+#fi
 
 function download(){
 	VERSION=$1
@@ -25,4 +25,4 @@ function download(){
 download "$APK_VERSION" "app-uiautomator.apk"
 download "$APK_VERSION" "app-uiautomator-test.apk"
 
-unzip -t app-uiautomator.apk && echo "$APK_VERSION" > apk_version.txt
+unzip -tq app-uiautomator.apk && echo "$APK_VERSION" > apk_version.txt
